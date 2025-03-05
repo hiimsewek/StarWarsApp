b60f02b190610911c540c429010860609a78885d
hiimsewek <62641653+hiimsewek@users.noreply.github.com>
2025-03-05 17:34:34 +0100

    Update GlobalStyle

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  :root {
+    --headerHeight: 70px;
+    --iconSize: 2.4rem;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -12,9 +17,33 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
+    overflow-x: hidden;
+  }
+
+  div#root {
+    display: flex;
+    flex-direction: column;
     min-height: 100vh;
     background-color: ${({ theme: { colors } }) => colors.gray800};
     color: ${({ theme: { colors } }) => colors.white};
-    font-family: ${({ theme: { font } }) => font}
+    font-family: ${({ theme: { font } }) => font};
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${({ theme: { colors } }) => colors.white};
+    transition: opacity 0.3s linear;
+    
+     &:hover {
+      opacity: 0.7
+     }
+  }
+
+  .scrollBlock {
+    overflow-y: hidden;
   }
 `;
